360ed37fc97d1f401ded7a7ebe33969ce8766632
tenzin kunsang <tenkus47@gmail.com>
2020-10-28 17:03:36 +0530

    initial start

---
+++ build/precache-manifest.73cc9bc288b787d0f6c946cbfc3094d6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee9c59d639d085e38b7508c89d5d6e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2361aaab21c46038609d",
+    "url": "/static/css/2.e6ac38db.chunk.css"
+  },
+  {
+    "revision": "a78ecc3f3014f075a316",
+    "url": "/static/css/main.6b40d67a.chunk.css"
+  },
+  {
+    "revision": "2361aaab21c46038609d",
+    "url": "/static/js/2.519a4eb5.chunk.js"
+  },
+  {
+    "revision": "ba47223459916d9157745da1332c93de",
+    "url": "/static/js/2.519a4eb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a78ecc3f3014f075a316",
+    "url": "/static/js/main.4e9fbb79.chunk.js"
+  },
+  {
+    "revision": "933f97f12e8acf994a3b",
+    "url": "/static/js/runtime-main.75347287.js"
+  },
+  {
+    "revision": "e6127078f857057f024a5532acdfd02f",
+    "url": "/static/media/1.e6127078.jpg"
+  },
+  {
+    "revision": "53d9a81f98ddde2caa9efc5510960812",
+    "url": "/static/media/2.53d9a81f.jpg"
+  },
+  {
+    "revision": "2bbc0cb8d9ce2378905246588760ae57",
+    "url": "/static/media/3.2bbc0cb8.jpg"
+  },
+  {
+    "revision": "07833c9884122358afadc1e566a922f6",
+    "url": "/static/media/5.07833c98.jpg"
+  }
+]);
